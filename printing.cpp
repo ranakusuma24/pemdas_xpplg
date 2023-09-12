67e42bd6ca754c5580ce5f41082843c985b1ec3d
ranakusuma24 <53632643+ranakusuma24@users.noreply.github.com>
2023-09-12 07:57:12 +0700

    kolaborasi printing.cpp

--- printing.cpp
+++ printing.cpp
@@ -1,10 +1,10 @@
 #include <iostream> 
 
 int main(){
-	std::cout << "halo sodara-sodara sekalian\n";
-	std::cout << "ini adalah baris baru.";
-	std::cout << "ini bukan baris baru.";
-	std::cout << " ini akhir kalimat." << std::endl;
+	std::cout << "hello student on X PPLG\n";
+	std::cout << This is my example file repository.";
+	std::cout << "you can try add this raw with your name to complete your group.";
+	std::cout << " now, I can know your name in the group" << std::endl;
 	std::cin.get();
 	return 0;
 }
